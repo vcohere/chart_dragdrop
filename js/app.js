c5b52f7dccec13d40b02162e84220d901bb9ae05
Valentin COHERE <v.cohere@gop.link>
2016-02-19 17:13:20 +0100

    can drag graph, size is updating

--- js/app.js
+++ js/app.js
@@ -10,29 +10,52 @@
 		}, options);
 		var dragged = null;
 
-		this.on('dragstart', function(e) {
-			dragged = $(this);
-		});
-		this.on('dragover', function(e) {
-			e.preventDefault();
-		}).on('drop', function(e) {
-			var elementClass = e.target.getAttribute('class'),
-				parentId = e.target.parentNode.getAttribute('id');
+		var cursorIn = function(e) {
+			var res;
 
-			e.preventDefault();
-			if (parentId === config.defaultZoneId)
-				dragged.html(config.beforeHTML);
-			else if (parentId === config.graphZoneId)
-				dragged.html(config.afterHTML);
-			else
-				dragged.html(config.afterHTML);
-			$(this).append(dragged)
+			$('.block').each(function() {
+				var offset = $(this).offset();
+				offset.right = offset.left + $(this).width();
+				offset.bottom = offset.top + $(this).height();
+
+				if (e.pageX < offset.right && e.pageX > offset.left &&
+					e.pageY > offset.top && e.pageY < offset.bottom) {
+					res = $(this);
+					$(this).append($('.elem.dragging'));
+					$('.elem.dragging').attr('style', '').removeClass('dragging');
+					window.dispatchEvent(new Event('resize'));
+					return false;
+				}
+			});
+			$('.elem.dragging').attr('style', '').removeClass('dragging');
+		};
+
+		this.mousedown(function() {
+			dragged = $(this);
+			dragged.closest('.elem').addClass('dragging');
+
+			$('#chart_div').mousemove(function(e) {
+				if (e.buttons !== 1) {
+					$('#chart_div').unbind();
+					return false;
+				}
+				window.getSelection().removeAllRanges();
+				dragged.closest('.elem').css({top: e.pageY - (dragged.height() / 2), left: e.pageX - (dragged.width() / 2), position: 'fixed'});
+			});
+		});
+
+		$(document).mouseup(function(e) {
+			if ($('.elem.dragging').length > 0) {
+				cursorIn(e);
+				$('#chart_div').unbind();
+				$('.elem.dragging').removeClass('dragging');
+			}
 		});
 	}
 })(jQuery);
 
 $(document).ready(function() {
-	$('#bar_chart').highcharts({
+	var options = {
 		chart: {
 			type: 'bar'
 		},
@@ -46,6 +69,11 @@ $(document).ready(function() {
 			name: 'Plop',
 			data: [2, 6, 3]
 		}]
-	});
-	$('.block').drag();
+	};
+
+	$('#bar_chart').highcharts(options);
+	$('#bar_chart2').highcharts(options);
+	$('#bar_chart3').highcharts(options);
+
+	$('.elem .drag_handle').drag();
 });
